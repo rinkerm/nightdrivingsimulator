c11f4e60fe996ee381773efb441e33bea454b3d1
rinkerm <rinkerm915@gmail.com>
2019-03-13 04:21:59 -0400

    GUI work

--- Night Driving Simulator/Assets/Scripts/Radio.cs
+++ Night Driving Simulator/Assets/Scripts/Radio.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class Radio : MonoBehaviour
 {
+
+    public float barDisplay; //current progress
+    public Vector2 pos = new Vector2(330, 500);
+    public Vector2 size = new Vector2(60, 20);
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+    private static GUIStyle fullbar;
+
+    void OnGUI()
+    {
+        if (fullTex == null)
+        {
+            fullTex = new Texture2D(1, 1);
+        }
+
+        if (fullbar == null)
+        {
+            fullbar = new GUIStyle();
+        }
+
+        fullTex.SetPixel(0, 0, Color.blue);
+        fullTex.Apply();
+
+        fullbar.normal.background = fullTex;
+        //draw the background:
+        GUI.Label(new Rect(pos.x, pos.y - 20, size.x, size.y), "Radio");
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyTex);
+
+        //draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), GUIContent.none, fullbar);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
     private Color startcolor = Color.white;
     public Player_Status p_status;
+    private float radioCD = 0f;
+    private int radioCDL = 60;
 
     void OnMouseEnter()
     {
@@ -21,9 +59,19 @@ public class Radio : MonoBehaviour
         ChangeRadio();
     }
 
+    void Update()
+    {
+        barDisplay = radioCD / 60f;
+        radioCD -= Time.deltaTime;
+    }
+
     void ChangeRadio()
     {
-        //music stuff
-        p_status.LowerFatigue();
+        if (radioCD < 0)
+        {
+            //translatewindow
+            p_status.LowerFatigue();
+            radioCD = radioCDL;
+        }
     }
 }
--- Night Driving Simulator/Assets/Scripts/Window.cs
+++ Night Driving Simulator/Assets/Scripts/Window.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class Window : MonoBehaviour
 {
+
+    public float barDisplay; //current progress
+    public Vector2 pos = new Vector2(240, 500);
+    public Vector2 size = new Vector2(60, 20);
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+    private static GUIStyle fullbar;
+
+    void OnGUI()
+    {
+        if (fullTex == null)
+        {
+            fullTex = new Texture2D(1, 1);
+        }
+
+        if (fullbar == null)
+        {
+            fullbar = new GUIStyle();
+        }
+
+        fullTex.SetPixel(0, 0, Color.blue);
+        fullTex.Apply();
+
+        fullbar.normal.background = fullTex;
+        //draw the background:
+        GUI.Label(new Rect(pos.x, pos.y - 20, size.x, size.y), "Window");
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyTex);
+
+        //draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), GUIContent.none, fullbar);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+
     public Player_Status p_status;
     private Color startcolor = Color.white;
-
+    private float windowCD = 0f;
+    private int windowCDL = 20;
     void OnMouseEnter()
     {
         GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
@@ -21,9 +59,19 @@ public class Window : MonoBehaviour
         MoveWindow();
     }
 
+    void Update()
+    {
+        barDisplay = windowCD / 20f;
+        windowCD -= Time.deltaTime;
+    }
+
     void MoveWindow()
     {
-        //translatewindow
-        p_status.LowerFatigue();
+        if (windowCD < 0)
+        {
+            //translatewindow
+            p_status.LowerFatigue();
+            windowCD = windowCDL;
+        }
     }
 }
